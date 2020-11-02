dbb18c0dea710054dafefb4102fa4bc12b538bdf
Cheng <chengjohnsonhs@gmail.com>
2020-11-02 16:12:37 +0800

    💫 UPDATE: mobile width: fit-content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../src/components/Navbar";
 import Footer from "../src/components/Footer";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import GoToTop from "../src/components/GoToTop";
 
 import { PageTransition } from "next-page-transitions";
